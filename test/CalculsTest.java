2f84cc0a3f5a3293a833d763285c81d72724a439
Abdoulaye BA <evilbas103848@gmail.com>
2022-02-22 16:31:52 +0100

    Remove method division in class CalculsTest

--- test/CalculsTest.java
+++ test/CalculsTest.java
@@ -1,5 +1,3 @@
-
-
 import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -22,7 +20,6 @@ public class CalculsTest {
 		c2 = new Calculs(10,20);
 		c3 = new Calculs(100,200);
 	}
-
 	
 
 	/**
@@ -77,27 +74,7 @@ public class CalculsTest {
 	/**
 	 * Test method for {@link Calculs#diviser()}.
 	 */
-	@Test
-	void testDiviser() {
-		
-		if(c1.diviser() != 0.5) {
-			
-			fail("Methode diviser non conforme 1/2=0.5");
-		}
-		
-		if (c2.diviser() != 0.5)
-		{
-			fail("Methode diviser non conforme 10/20=0.5 ne fonctionne pas.");			
-		}
-		if (c3.diviser() != 0.5)
-		{
-			fail("Methode diviser non conforme 100/200=0.5 ne fonctionne pas.");			
-		}
-		
-		// Est ce que (2 == 42) ? non donc : test en Erreur
-		assertEquals(c1.diviser(),0.5, "Methode additionner non conforme 1/2=0.5 ne fonctionne pas");
-	}
-
+	
 	/**
 	 * Test method for {@link Calculs#soustraire()}.
 	 */
